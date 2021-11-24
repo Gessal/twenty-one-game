99d9c3f82242cc6593865ddd2dee61ca49b41816
Gessal Maxim <gessalmaxim@gmail.com>
2021-11-24 11:59:12 +0300

    Первый шаг

---
+++ src/test/java/com/example/twentyonegame/TwentyOneGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.twentyonegame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwentyOneGameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
